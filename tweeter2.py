00f160834cfffd9ed2666dbee6620497d27618a5
Anshulguptag <ganshul9172gmail.com>
2018-03-19 10:05:43 +0530

    Sentiment Analysis

--- tweeter2.py
+++ tweeter2.py
@@ -3,7 +3,9 @@ import numpy as np
 import pandas as pd
 import datetime
 import re
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from datetime import date, timedelta
+import nltk
 ####input your credentials here
 consumer_key = 'AYDNqr9ycBI9qaOWoYXJgYnKY'
 consumer_secret = 'tSHVPOr6JrQ9KNnqX1aSOGnKecmPeQQ37j81JAURI0t6deb8AA'
@@ -16,14 +18,12 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth,wait_on_rate_limit=True)
 #####United Airlines
-df=pd.read_csv('positive.txt', header=None)
-positive_vocab=np.concatenate(np.array(df), axis=0)
+#nltk.download('vader_lexicon')
+
 now=datetime.datetime.now()
 enddate=now.date()
 startdate=enddate - timedelta(10)
-df1=pd.read_csv('negative.txt', header=None)
-print (startdate, enddate)
-negative_vocab=np.concatenate(np.array(df1), axis=0)
+
 for tweet in tweepy.Cursor(api.search,q=hashtagtweet,count=100,
                            lang="en",
                            since=startdate, until=enddate).items():
@@ -32,30 +32,25 @@ for tweet in tweepy.Cursor(api.search,q=hashtagtweet,count=100,
     print (tweet.created_at, tweet.text)
     pos=0
     neg=0
+    neu=0
+
     sentence = tweet.text
-    '''
-    if ':' in sentence:
-        sentence = (sentence.rsplit(':')[1]).rsplit('.', 1)[0]
-    else:
-        sentence = sentence.rsplit('.', 1)[0]
-    '''
+    sid=SentimentIntensityAnalyzer()
     #sentence = sentence.replace('RT @', '')
     sentence = re.sub('[^ a-zA-Z0-9' ']', '', sentence)
     file.write('Tweet: '+str(tweet.created_at)+' : ' + str(sentence))
 
+
     sentence = sentence.lower()
-    words = (sentence).split(' ')
-    for word in words:
-        #print (word)
-        if word in positive_vocab:
-            pos+=1
-        elif word in negative_vocab:
-            neg+=1
-
-
-    if pos>neg:
+    sentence1=[tweet.text]
+    for s in sentence1:
+        ss = sid.polarity_scores(s)
+        pos=pos+ss['pos']
+        neg=neg+ss['neg']
+        neu=neu+ss['neu']
+    if pos>neg :
         d='Positive'
-    elif pos<neg:
+    elif pos<neg :
         d='Negative'
     else:
         d='Neutral'
